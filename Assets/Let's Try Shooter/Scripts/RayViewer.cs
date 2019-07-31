672f38737c4ae3369a8a2c67208f53223ab3d541
Andrew_Yefimov <andrewwilliamsond@gmail.com>
2019-07-31 19:24:23 +0300

    Full upload
    Adding all project files

---
+++ Assets/Let's Try Shooter/Scripts/RayViewer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayViewer : MonoBehaviour
+{
+
+	public float weaponRange = 50f;
+	private Camera fpsCam;
+
+	// Use this for initialization
+	void Start ()
+	{
+		fpsCam = GetComponentInParent<Camera> ();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 lineOrigin = fpsCam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 0.0f));
+		Debug.DrawRay (lineOrigin, fpsCam.transform.forward * weaponRange, Color.green);
+	}
+}
---
+++ Assets/Let's Try Shooter/Scripts/RaycastShoot.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityScript.Steps;
+using System.Diagnostics;
+
+public class RaycastShoot : MonoBehaviour
+{
+	public int gunDamage = 1;
+	public float fireRate = .25f;
+	public float weaponRange = 50f;
+	public float hitForce = 100f;
+	public Transform gunEnd;
+
+	private Camera fpsCam;
+	private readonly WaitForSeconds shotDuration = new WaitForSeconds (0.07f);
+	private AudioSource gunAudio;
+	private LineRenderer laserLine;
+	private float nextFire;
+
+	// Use this for initialization
+	void Start ()
+	{
+		laserLine = GetComponent<LineRenderer> ();
+		gunAudio = GetComponent<AudioSource> ();
+		fpsCam = GetComponentInParent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			StartCoroutine (ShotEffect ());
+			Vector3 rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 0));
+			RaycastHit hit;
+			laserLine.SetPosition (0, gunEnd.position);
+			if (Physics.Raycast (rayOrigin, fpsCam.transform.forward, out hit, weaponRange)) {
+				laserLine.SetPosition (1, hit.point);
+				ShootableBox health = hit.collider.GetComponent<ShootableBox> ();
+				if (health != null)
+					health.Damage (gunDamage);
+				if (hit.rigidbody != null)
+					hit.rigidbody.AddForce (-hit.normal * hitForce);
+			} else {
+				laserLine.SetPosition (1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+			}
+		}
+	}
+
+	private IEnumerator ShotEffect ()
+	{
+		gunAudio.Play ();
+		laserLine.enabled = true;
+		yield return shotDuration;
+		laserLine.enabled = false;
+	}
+}
